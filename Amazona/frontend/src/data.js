6508a2c6955b13d62a40ce6caab99218a4a0104c
YouriDK <youri.choucoutou@gmail.com>
2020-11-30 17:20:27 +0100

    [FEAT]  Passage-React

---
+++ Amazona/frontend/src/data.js
@@ -0,0 +1,31 @@
+export default {
+  products: [
+    {
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "/images/d1.jpg",
+      price: 60,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+    },
+    {
+      name: "Slim Shirt",
+      category: "Shirts",
+      image: "/images/d1.jpg",
+      price: 90,
+      brand: "Nike",
+      rating: 5,
+      numReviews: 20,
+    },
+    {
+      name: "Pants jogging",
+      category: "Pants",
+      image: "/images/d1.jpg",
+      price: 80,
+      brand: "Nike",
+      rating: 3,
+      numReviews: 15,
+    },
+  ],
+};
