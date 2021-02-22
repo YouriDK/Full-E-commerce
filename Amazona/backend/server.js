f84ca8d6ceca6c9167d6139367f7c9aefb579761
YouriDK <youri.choucoutou@gmail.com>
2021-02-22 16:04:10 +0100

    Heroku try

--- Amazona/backend/server.js
+++ Amazona/backend/server.js
@@ -20,12 +20,25 @@ mongoose
   })
   .catch((error) => console.log(error.reason));
 
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected ! ");
+});
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/product", productRoute); // ! Il fallait mettre cela pour pouvoir voir les détais du produit
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
 app.get("/api/config/paypal", (req, res) => {
   //* Pour récupérer le client ID paypal dans le back end
@@ -35,6 +48,10 @@ app.get("/api/config/paypal", (req, res) => {
 /* 
 ! express understand only ES5 and we're in ES6  that's why we need babel*/
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
+
 app.listen(5000, () => {
-  console.log("Server started at http://localhost:5000");
+  console.log(`Serve at http://localhost:${PORT}`);
 });
