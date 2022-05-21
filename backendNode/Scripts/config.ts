3d3645884815398cabb7890d494c250a3c1fe3aa
Youri Choucoutou <ychoucoutou@amiltone.com>
2022-05-21 15:52:26 +0200

    [DELETE] Express backend

--- backendNode/Scripts/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  MONGODB_URI:
-    'mongodb+srv://newUser123:kaN22Ru4C@cluster0.jtafi.mongodb.net/eShop?retryWrites=true&w=majority',
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost',
-  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-};
