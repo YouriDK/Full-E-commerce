fd72af38e44e11ef97cbf42398410f9af388d625
Youri Choucoutou <ychoucoutou@amiltone.com>
2022-08-23 11:45:43 +0200

    fix path

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../../app/users/users.module';
+import { UsersModule } from '../app/users/users.module';
 import { AuthService } from './auth.service';
 
 @Global() // ! Allow to use the Service everywhere 👌
