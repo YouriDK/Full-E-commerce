732cba75f538f29b129b1d4b8dc15cd11bfb817b
Youri Choucoutou <ychoucoutou@amiltone.com>
2022-08-30 11:01:12 +0200

    ...

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -11,7 +11,7 @@ export class AppController {
 
   @Get()
   getHello(@Res() res: any): string {
-    console.log('Hello', __dirname, '../../frontend/build/index.html');
+    console.log('PATH', __dirname, '../../frontend/build/index.html');
     res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
     return this.appService.getStart();
   }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
-
+import path from 'path';
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:5000', 'http://localhost:3000'],
   });
+  app.setGlobalPrefix('api');
+  app.use(path.join(__dirname, '../../frontend/build/index.html'));
 
   await app.listen(process.env.PORT ?? 5000);
 }
