ee2e15f96f9e9cad3577a7232484249f2d55398a
Youri Choucoutou <ychoucoutou@amiltone.com>
2022-08-30 15:14:23 +0200

    ...

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
 import path from 'path';
 config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: ['http://localhost:5000', 'http://localhost:3000'],
   });
 
   app.setGlobalPrefix('api');
   //app.use('*', path.join(__dirname, '../../frontend/build/index.html'));
-  app.use('*', (res: any) => {
-    res.sendFile(path.join(__dirname, '../../frontend/build/index.html'));
-  });
+  app.useStaticAssets(path.join(__dirname, '../../frontend/build/index.html'));
 
   await app.listen(process.env.PORT ?? 5000);
 }
